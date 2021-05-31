5a415cc1c33cf82bac3ba09131e7c6ed632dee17
Josuke <y.motosugi0227@gmail.com>
2021-05-31 17:36:38 +1000

    Make position of snack bar responsive to the size of the screen.

--- src/components/shared/CustomSnackBar.jsx
+++ src/components/shared/CustomSnackBar.jsx
@@ -1,17 +1,30 @@
+import { useTheme } from "@material-ui/core/styles";
 import Snackbar from "@material-ui/core/Snackbar";
 import Alert from "@material-ui/lab/Alert";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 const CustomSnackBar = ({ showSnackBar, setShowSnackBar }) => {
+  const theme = useTheme();
+  const matches = useMediaQuery(theme.breakpoints.up("sm"));
+
   const { editing, show, severity, message } = showSnackBar;
 
   let anchorOigin = editing
     ? { vertical: "bottom", horizontal: "center" }
     : { vertical: "top", horizontal: "center" };
 
+  let anchorOriginLaptop = editing
+    ? { vertical: "top", horizontal: "right" }
+    : { vertical: "top", horizontal: "left" };
+
+  const setAnchorOrigin = () => {
+    return matches ? anchorOriginLaptop : anchorOigin;
+  };
+
   return (
     <Snackbar
       open={show}
-      anchorOrigin={anchorOigin}
+      anchorOrigin={setAnchorOrigin()}
       autoHideDuration={6000}
       onClose={() => setShowSnackBar({ ...showSnackBar, show: false })}
     >
