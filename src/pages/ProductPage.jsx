c9132aa1b6109688b2b2cd1d73902e0984203eb6
Josuke <y.motosugi0227@gmail.com>
2021-06-21 22:19:00 +1000

    Add delete feature to product

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -17,32 +17,54 @@ import { useSelector } from "react-redux";
 import { getProducts, deleteProduct } from "../services/productServices";
 
 import ProductCardForEditing from "../components/ProductCardForEditing";
+import { isEqual } from "../util/isEqual";
 
 const useStyles = makeStyles({});
 
-// const products = [
-//   {
-//     sold: 0,
-//     _id: "60c768df93fe691d348a2276",
-//     title: "Test",
-//     price: 1,
-//     category: {
-//       _id: "60b820a07468c922cf38beb7",
-//       name: "Books",
-//       slug: "Books",
-//       createdAt: "2021-06-03T00:21:52.278Z",
-//       updatedAt: "2021-06-08T07:05:06.449Z",
-//       __v: 0,
-//     },
-//     quantity: 10,
-//     color: "white",
-//     brand: "",
-//     description: "<p>test</p>\n",
-//     slug: "test",
-//     ratings: [],
-//     __v: 0,
-//   },
-// ];
+const productsSample = [
+  {
+    sold: 0,
+    _id: "60c768df93fe691d348a2278",
+    title: "Test",
+    price: 1,
+    category: {
+      _id: "60b820a07468c922cf38beb7",
+      name: "Books",
+      slug: "Books",
+      createdAt: "2021-06-03T00:21:52.278Z",
+      updatedAt: "2021-06-08T07:05:06.449Z",
+      __v: 0,
+    },
+    quantity: 10,
+    color: "white",
+    brand: "",
+    description: "<p>test</p>\n",
+    slug: "test",
+    ratings: [],
+    __v: 0,
+  },
+  {
+    sold: 0,
+    _id: "60c768df93fe691d348a2276",
+    title: "Test",
+    price: 1,
+    category: {
+      _id: "60b820a07468c922cf38beb7",
+      name: "Books",
+      slug: "Books",
+      createdAt: "2021-06-03T00:21:52.278Z",
+      updatedAt: "2021-06-08T07:05:06.449Z",
+      __v: 0,
+    },
+    quantity: 10,
+    color: "white",
+    brand: "",
+    description: "<p>test</p>\n",
+    slug: "test",
+    ratings: [],
+    __v: 0,
+  },
+];
 
 const ProductPage = ({ location }) => {
   const [products, setProducts] = useState([]);
@@ -61,7 +83,16 @@ const ProductPage = ({ location }) => {
   };
 
   const handleProductDelete = async (product) => {
-    await deleteProduct(product, user);
+    try {
+      await deleteProduct(product, user);
+      let current = [...products];
+      for (let i = 0; i < current.length; i++) {
+        if (isEqual(product, current[i])) current.splice(i, 1);
+      }
+      setProducts(current);
+    } catch (error) {
+      console.log("product delete error", error);
+    }
   };
 
   return (
