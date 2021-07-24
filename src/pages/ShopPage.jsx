1846e5365b95534b4b8f03d53f1db444467af50e
Josuke <y.motosugi0227@gmail.com>
2021-07-24 17:50:02 +1000

    Apply pagination to shop page.

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -3,11 +3,19 @@ import { useSelector } from "react-redux";
 import { Container, Grid } from "@material-ui/core";
 import Layout from "../components/Layout";
 import ProductCard from "../components/ProductCard";
-import { filterByAttribute, getProducts } from "../services/productServices";
+import Pagination from "@material-ui/lab/Pagination";
+import {
+  filterByAttribute,
+  getProducts,
+  getProductsList,
+  getProductsCount,
+} from "../services/productServices";
 
 const ShopPage = ({ location }) => {
   const { query } = useSelector((state) => ({ ...state }));
   const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+  const [count, setCount] = useState(1);
 
   useEffect(() => {
     if (!query.length) {
@@ -17,12 +25,41 @@ const ShopPage = ({ location }) => {
     loadFilteredProducts(query);
   }, [query]);
   const loadWholeProducts = async () => {
-    const { data } = await getProducts();
+    const { data } = await getProductsList("createdAt", "desc", page, 2);
     setProducts(data);
   };
   const loadFilteredProducts = async () => {
     const { data } = await filterByAttribute(query);
     setProducts(data);
+  };
+
+  useEffect(() => {
+    loadProducts();
+  }, [page]);
+  const loadProducts = async () => {
+    try {
+      const { data } = await getProductsList("createdAt", "desc", page, 2);
+      setProducts(data);
+    } catch (error) {
+      console.log("product fetching error", error);
+    }
+  };
+
+  useEffect(() => {
+    loadCount();
+  }, []);
+  const loadCount = async () => {
+    try {
+      const { data } = await getProductsCount();
+      console.log(data);
+      setCount(data);
+    } catch (error) {
+      console.log("product count fetching error", error);
+    }
+  };
+
+  const handleChange = (event, value) => {
+    setPage(value);
   };
 
   return (
@@ -33,6 +70,7 @@ const ShopPage = ({ location }) => {
             <ProductCard key={p._id} product={p} />
           </Grid>
         ))}
+        <Pagination count={count / 2} page={page} onChange={handleChange} />
       </Grid>
     </Layout>
   );
