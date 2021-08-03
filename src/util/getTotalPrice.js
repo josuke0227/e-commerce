f818bb6a903953416e0d32347c1f0fd0b060fc42
Josuke <y.motosugi0227@gmail.com>
2021-08-03 19:19:24 +1000

    Delete unused code.

--- src/util/getTotalPrice.js
+++ src/util/getTotalPrice.js
@@ -1,5 +1,4 @@
 export const getTotalPrice = (cart) => {
-  console.log(cart);
   if (!cart.length) return 0;
   if (cart.length === 1) return cart[0].price * cart[0].quantity;
   return cart.reduce(
